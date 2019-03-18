1990e72c5034f3dc5cc3d58537bd0de6fc847ca9
moueza <mouezapeter@gmail.com>
2019-03-18 10:38:10 +0100

    #7 routes will

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { WillisDieHardUneJourneeEnEnferFilmComponent } from './willis-die-hard-une-journee-en-enfer-film/willis-die-hard-une-journee-en-enfer-film.component';
+const routes: Routes = [
+  { path: 'will', component: WillisDieHardUneJourneeEnEnferFilmComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
