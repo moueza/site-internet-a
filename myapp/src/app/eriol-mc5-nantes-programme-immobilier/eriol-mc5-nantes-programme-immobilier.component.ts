cef8e8ea0b15df7ad0b126411e3cf112422fd2a7
moueza <mouezapeter@gmail.com>
2019-05-18 10:35:31 +0200

    See #13 Indexation SEO ; meta keywords

--- myapp/src/app/eriol-mc5-nantes-programme-immobilier/eriol-mc5-nantes-programme-immobilier.component.ts
+++ myapp/src/app/eriol-mc5-nantes-programme-immobilier/eriol-mc5-nantes-programme-immobilier.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-eriol-mc5-nantes-programme-immobilier',
   templateUrl: './eriol-mc5-nantes-programme-immobilier.component.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EriolMC5NantesProgrammeImmobilierComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meta: Meta) { 
+     this.meta.addTag({ name: 'description', content: 'Nantes Malakoff Renouveau' });
+    this.meta.addTag({ name: 'author', content: 'Peter MOUEZA' });
+    this.meta.addTag({ name: 'keywords', content: 'peter, moueza, Nantes,Malakoff, immobilier,Eriol,MC5,programme,immeuble,architecture' });
+	}
 
   ngOnInit() {
   }
--- myapp/src/app/images/images.component.ts
+++ myapp/src/app/images/images.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-images',
   templateUrl: './images.component.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meta: Meta) { 
+     this.meta.addTag({ name: 'description', content: 'Image de vaisseau sanguin' });
+    this.meta.addTag({ name: 'author', content: 'Peter MOUEZA' });
+    this.meta.addTag({ name: 'keywords', content: 'peter, moueza, Nantes,sang, blood,biology,medicine,red,cells,art' });
+}
 
   ngOnInit() {
   }
