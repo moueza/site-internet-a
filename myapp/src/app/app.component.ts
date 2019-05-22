eb652eed9fe36e2b1bb32b837d4612b676c09153
moueza <mouezapeter@gmail.com>
2019-05-22 20:26:41 +0200

    meta in main component

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  
+  
+  constructor(private meta: Meta) { 
+     this.meta.addTag({ name: 'description', content: 'Site principal de Peter MOUEZA' });
+    this.meta.addTag({ name: 'author', content: 'Peter MOUEZA' });
+    this.meta.addTag({ name: 'keywords', content: 'peter, moueza, Nantes' });
+}
 }
